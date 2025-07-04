4e0454f178b17c9ff06cc8b7742fc9a0c12011a2
Aashu patil <aashupatil218@gmail.com>
2025-07-04 19:18:44 +0530

    middelwares-document,query,aggregate...

--- models/tourmodel.js
+++ models/tourmodel.js
@@ -1,65 +1,115 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'name is required'],
-    unique: true,
-    trim: true,
-  },
-  duration: {
-    type: Number,
-    required: true,
-  },
-  maxGroupSize: {
-    type: Number,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    default: 3.5,
-  },
-  difficulty: {
-    type: String,
-    required: true,
-  },
-  ratingsAverage: {
-    type: Number,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'name is required'],
+      unique: true,
+      trim: true,
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: true,
+    },
+    maxGroupSize: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 3.5,
+    },
+    difficulty: {
+      type: String,
+      required: true,
+    },
+    ratingsAverage: {
+      type: Number,
 
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
 
-    default: 0,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    primeDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
+    secretour: { type: Boolean, default: false },
   },
-  price: {
-    type: Number,
-    required: true,
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: false },
   },
-  primeDiscount: {
-    type: Number,
-  },
-  summary: {
-    type: String,
-    trim: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: true,
-  },
-  images: {
-    type: [String],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
+);
+
+// tourSchema.virtual('durationweeks').get(function () {
+//   return this.duration / 7;
+// });
+
+// tourSchema.pre('save', function (next) {
+//   this.slug=slugify(this.name, { lower:true });
+//   next();
+// });
+
+// tourSchema.post('save', function (doc,next) {
+//   console.log(doc);
+
+//   next();
+// });
+
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+// tourSchema.post(/^find/, function (docs, next) {
+//   console.log(`query took ${Date.now()-this.start} milisec`);
+
+//   //console.log(docs);
+
+//   next();
+// });
+
+// tourSchema.pre('findOne', function (next) {
+//   this.find({ secretour: { $ne: true } });
+//   next();
+// });
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretour: { $ne: true } } });
+  console.log(this);
+  next();
 });
 
 const Tour = new mongoose.model('Tour', tourSchema);
--- routes/tourroutes.js
+++ routes/tourroutes.js
@@ -5,13 +5,15 @@ const {
   AddTour,
   updatetour,
   deleteTour,
-  aliasTopTours
- 
+  aliasTopTours,
+  getTourStats,
+  getMonthlyPlan
 } = require('./../controller/tourcontroller');
 const router = express.Router();
 
-
-router.route('/top-5-cheap').get(aliasTopTours,getAllTours)
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(getAllTours).post(AddTour);
 router.route('/:id').get(getSingleTour).patch(updatetour).delete(deleteTour);
 
